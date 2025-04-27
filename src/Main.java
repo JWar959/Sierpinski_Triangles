8f3a5b31294a03e472b7c765a083c19b5b88a447
JWar959 <john.warren959@gmail.com>
2025-04-27 09:37:02 -0400

    Implement main function, attaching a panel onto frame.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		// create frame
+		JFrame frame = new JFrame("Sierpinski Triangle");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Instantiate an object 
+		TrianglePanel panel = new TrianglePanel();
+		
+		// set the size of the panel
+		panel.setPreferredSize(new java.awt.Dimension(600,600));
+		
+		// add the panel to the frame
+		frame.getContentPane().add(panel);
+		
+		// invoke pack to ensure proper sizing
+		frame.pack();
+		frame.setVisible(true);
 
 	}
 
