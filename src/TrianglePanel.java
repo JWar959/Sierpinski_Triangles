2dd2300f9d81e97c442086beeaae94c36e30f9e4
JWar959 <john.warren959@gmail.com>
2025-04-27 10:02:08 -0400

    Implement recursive draw function drawTriangle and function findMIddle to compute and return the location of the middle between two Points.

--- src/TrianglePanel.java
+++ src/TrianglePanel.java
@@ -23,7 +23,33 @@ public class TrianglePanel extends JPanel {
 	}
 	
 	private void drawTriangle(Graphics2D g2d, Point p1, Point p2, Point p3, int iterations) {
+		// set up the base case to stop the recursion
+		if(iterations == 0) {
+			int[] xPoints = {p1.x, p2.x, p3.x};
+			int[] yPoints = {p1.y, p2.y, p3.y};
+			g2d.fillPolygon(xPoints, yPoints, 3);
+			
+			// return to caller and halt the recursion
+			return;
+		}
 		
+		// First thing we'll need to do is to locate the midpoints
+		Point mid1 = findMiddle(p1, p2);
+		Point mid2 = findMiddle(p2, p3);
+		Point mid3 = findMiddle(p3,p1);
+		
+		// Recursively draw the three smaller triangles
+		drawTriangle(g2d, p1, mid1, mid3, iterations - 1);
+		drawTriangle(g2d, mid1, p2, mid2, iterations - 1);
+		drawTriangle(g2d, mid3, mid2, p3, iterations - 1);		
+	}
+	
+	private Point findMiddle(Point p1, Point p2) {
+		int middleX = (p1.x + p2.x) / 2;
+		int middleY = (p1.y + p2.y) / 2;
+		
+		// return the new middle point back to the caller
+		return new Point(middleX, middleY);
 	}
 
 }
